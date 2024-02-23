c25ac34f9ea279860667648ba727dc06d654c6ae
Mohamed Sayed <160779962+mohamedsayedhus@users.noreply.github.com>
2024-02-23 19:14:22 +0200

    SQL Sales Project.sql

--- SQL Sales Project.sql
+++ SQL Sales Project.sql
@@ -3,19 +3,19 @@ USE SalesEcommerce
 SELECT  max (Sales)
 FROM [US  E-commerce records 2020]
 
-SELECT *
+SELECT Customer_ID
 FROM [US  E-commerce records 2020]
 
 --**In The Beginning 
 -- SUM Total (Sales - Quantity - Profit )
 
-SELECT STR(SUM(Sales)) AS TotalSales
+SELECT (SUM(Sales)) AS TotalSales
 FROM [US  E-commerce records 2020]
 
-SELECT STR(SUM (Quantity)) AS TotalQuantitys
+SELECT (SUM (Quantity)) AS TotalQuantitys
 FROM [US  E-commerce records 2020]
 
-SELECT STR(SUM (Profit)) AS TotalProfit
+SELECT (SUM (Profit)) AS TotalProfit
 FROM [US  E-commerce records 2020]
 
 -- Top City By Ordared 
@@ -26,13 +26,15 @@ ORDER BY Quantity DESC
 
 -- Top State By Ordared 
 
-SELECT State, Quantity, Sales
+SELECT State, SUM(Quantity) AS TotalQuantity, SUM(Sales) AS TotalSales
 FROM [US  E-commerce records 2020]
-ORDER BY Quantity DESC 
+GROUP BY State
+ORDER BY State DESC;
+
 
 -- Selling categories
 
-SELECT Category, str(Sales) AS TotalSales
+SELECT Category, (Sales) AS TotalSales
 FROM [US  E-commerce records 2020]
 ORDER BY 2 DESC
 -- Final Gode 
@@ -49,14 +51,14 @@ GROUP BY Product_Name
 ORDER BY Total_P_Qunatity DESC
 
 -- Sales distribution
-SELECT STR(Min(Sales)) As TotalSales, STR(AVG(Sales)), STR(MAX(Sales))
+SELECT (Min(Sales)) As TotalSales, (AVG(Sales)), (MAX(Sales))
 FROM [US  E-commerce records 2020]
 
-SELECT STR(Sales) As TotalSales, Category,
+SELECT (Sales) As TotalSales, Category,
 		CASE 
 			WHEN Sales <= 1000 THEN 'Less Then 1K'
-			WHEN Sales <= 1001 AND Sales <= 5000 THEN '1k To 5k'
-			WHEN Sales <= 5001  AND Sales <= 10000 THEN '5k To 10k'
+			WHEN Sales >= 1000 AND Sales <= 5000 THEN '1k To 5k'
+			WHEN Sales >= 5001  AND Sales <= 10000 THEN '5k To 10k'
 		  ELSE 'More than 10k'
 		END AS Sales_distribution
 FROM [US  E-commerce records 2020]
@@ -80,19 +82,27 @@ ORDER BY Sales_distribution DESC
 
 ----------------------------------------------------------
 -- CET BY Whatever I need 
-
-WITH CTE_Ecommerce as
-(
-	SELECT Customer_ID, Segment, City, Category, Ship_Mode
-			, COUNT (Customer_ID) OVER (PARTITION BY Quantity) AS Total_Cus_Quantity
-			, COUNT (Customer_ID) OVER (PARTITION BY Sales) AS Total_Cus_Sales
-	FROM [US  E-commerce records 2020]
-	WHERE Sales BETWEEN '1000' AND '14000'
-)
 ------------------------Here is the ranking according to the number of customers-----------------------------
-SELECT *
+
+WITH CTE_Ecommerce AS (
+    SELECT 
+        Customer_ID, 
+        Segment, 
+        City, 
+        Category, 
+        Ship_Mode,
+        COUNT(Customer_ID) OVER (PARTITION BY Quantity) AS Total_Cus_Quantity,
+        COUNT(Customer_ID) OVER (PARTITION BY Sales) AS Total_Cus_Sales
+    FROM [US  E-commerce records 2020]
+    WHERE Sales BETWEEN 1000 AND 14000
+)
+
+SELECT 
+    *,
+    DENSE_RANK() OVER (ORDER BY Total_Cus_Quantity DESC) AS Customer_Rank
 FROM CTE_Ecommerce
-ORDER BY Total_Cus_Quantity DESC 
+ORDER BY Total_Cus_Quantity DESC;
+
 
 ------------------------ Here is the grouping by category and Segment ------------------------------- 
 SELECT 
